a2806a592b2405afde3e75ac793f83b4e95bc78f
Nico <ednip7@gmail.com>
2021-01-22 10:22:01 -0300

    Imprime 1

--- get-quote.py
+++ get-quote.py
@@ -1,19 +1,11 @@
 def main():
     print("Keep it logically awesome.")
 
-<<<<<<< HEAD
     f = open("quotes.txt")
     quotes = f.readlines()
     f.close()
 
-  #print(quotes[0])
-=======
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
-
-  print(quotes[0])
->>>>>>> ac11da23616bda0e4cca74d1705072b4260b018c
+    print(quotes[0])
 
 if __name__== "__main__":
   main()
